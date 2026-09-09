dd71be3ca3935a4b6feb44c573f06af6dbf04c5c
payam <payamsiami90@gmail.com>
2026-09-09 10:25:14 +0330

    edit app

--- platform/packages/shared/src/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./types/index.js";
-export * from "./events/index.js";
-export * from "./lib/config.js";
-export * from "./lib/jwt.js";
-export * from "./lib/http.js";
-export * from "./lib/mongo.js";
-export * from "./lib/redis.js";
